e35033e522d94eca7d5277caf171766e281b87d5
Utkarsh Gaikwad <gaikwadujg@gmail.com>
2024-03-10 16:49:39 +0000

    Files added

---
+++ example.py
@@ -0,0 +1,25 @@
+from stackup_python import stackup
+import numpy as np
+
+size=100000
+dim1 = stackup.dimension(mean=30, tol=15, size=size)
+dim2 = stackup.dimension(mean=40, tol=20, size=size)
+
+seed = 42
+a = dim1.generate(seed)
+b = dim2.generate(seed)
+
+c = np.sqrt(a**2 + b**2)
+
+mu = np.mean(c)
+sigma = np.std(c)
+
+print(c)
+print(f'Average value of c : {mu:.2f}')
+print(f'Standard Deviation : {sigma:.2f}')
+print(f'Maximum : {mu+3*sigma:.2f}')
+print(f'Minimum : {mu-3*sigma:.2f}')
+
+stackup.plot_dist(c)
+
+stackup.fit_dist(c)
---
+++ stackup_python/stackup.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from fitter import Fitter,  get_common_distributions
+
+class dimension:
+
+    def __init__(self, mean, tol, size=10000):
+        self.mean = mean
+        self.tol = tol
+        self.size = size
+
+    def generate(self, random_seed=None):
+        if random_seed is not None:
+            np.random.seed(random_seed)
+            x = np.random.normal(loc=self.mean, scale=self.tol/3, size=self.size)
+        else:
+            x = np.random.normal(loc=self.mean, scale=self.tol/3, size=self.size)
+        return x
+
+def plot_dist(x):
+    plt.figure(figsize=(10, 5))
+    sns.histplot(x, kde=True)
+    plt.title('Histogram for Dimension')
+    plt.show()
+    plt.savefig('histogram.png')
+
+def fit_dist(x):
+    f = Fitter(x, distributions=get_common_distributions())
+    f.fit()
+    f.summary()
+    best_fit = f.get_best()
+    print(f'Best fitted distribution : {best_fit}')
+
+
+
